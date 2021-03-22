8ad959e0b327a5676d2417a710f9c2c619882a12
Benito <bbxblack@gmail.com>
2021-03-22 10:12:51 +0100

    esercizio live

--- js/script.js
+++ js/script.js
@@ -6,6 +6,27 @@ function sayHello() {
 
 function init() {
 
-  $
+  // $('#title').slideDown(5000);
+  // $('#title').slideUp(5000);
+
+  // $('#title').slideToggle(5000);
+  // $('#title').slideToggle(5000, function() {
+  //
+  //   console.log('end animation');
+  //
+  // });
+
+  $('#title1').slideToggle(5000, function() {
+
+    $('#title2').slideToggle(5000, function() {
+
+      $('#title3').slideToggle(5000);
+
+    });
+
+  });
+
 
 }
+
+ $(document).ready(init);
